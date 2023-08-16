9da7251235921bd7a7351d6696543eda0404df2d
Mahabub Alam <mahabubx7@gmail.com>
2023-08-16 14:40:43 +0600

    refactor (controllers): updated-controllers-for-actual-needs

--- app/controllers/balance_controller.rb
+++ app/controllers/balance_controller.rb
@@ -2,7 +2,29 @@ class BalanceController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @balances = Balance.where(author_id: current_user.id).includes([:items]).order('created_at DESC')
+    @balances = Balance.where(author_id: current_user.id).order('created_at DESC')
+  end
+
+  def new
+    @balance = Balance.new
+  end
+
+  def show
+    if current_user.id.to_i == Balance.find(params[:id]).author_id.to_i
+      @balance = Balance.find(params[:id])
+    else
+      redirect_to '/'
+    end
+  end
+
+  def create
+    @balance = Balance.new(balance_params)
+    @balance.author = current_user
+    if @balance.save
+      redirect_to root_path, notice: 'Balance was successfully created'
+    else
+      render :new, status: :unprocessable_entity, notice: 'An error has occurred while creating'
+    end
   end
 
   private
